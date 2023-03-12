19f4193f03a1a5669318cb34cf4bbfb9456d1836
anjilinux <anjireddy2024@gmail.com>
2023-03-12 14:53:08 +0530

    this is uat-CUSTEMER MODIFIED

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 @app.route("/")
 
 def anji():
-    return "helo this is dev environment"
+    return "helo this is UAT CUSTEMER FINAL environment"
 if __name__=="__main__":
     app.run(debug=True,port=5000)
